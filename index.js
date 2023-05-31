3158d66a3884421217f9710556b932bd4420ff60
Paresh <09patilparesh@gmail.com>
2023-05-31 10:08:48 +0530

    31

--- index.js
+++ index.js
@@ -77,17 +77,17 @@ closeIcon.addEventListener('click', () => {
 
  // Targeting video element 
  let clip = document.querySelector(".vid")
-  
- /* Applying mouseover event on video clip 
- and then we call play() function to play 
- the video when the mouse is over the video */
  clip.addEventListener("mouseover", function (e) {
      clip.play();
  })
-
- /* Applying mouseout event on video clip 
- and then we call pause() function to stop 
- the video when the mouse is out the video */
  clip.addEventListener("mouseout", function (e) {
      clip.pause();
  })
+
+
+ 
+
+   $(window).load(function() {
+   $(".preloader").delay(5000).fadeOut("slow");
+   $("#overlayer").delay(5000).fadeOut("slow");
+   })
